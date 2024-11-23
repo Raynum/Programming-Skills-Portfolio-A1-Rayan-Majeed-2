246d6953a48b946b90d79cd46b296d6e53a03b5d
Rayan Majeed <rayanmajeed91@gmail.com>
2024-11-23 19:47:57 +0400

    Update redone excercise 3.py

--- redone excercise 3.py
+++ redone excercise 3.py
@@ -1,5 +1,5 @@
 #Exercise 3: Biography
 Biography= {"Name": "Rayan Majeed", "Hometown": "Islamabad", "Age": 18}
 
-for items, item in A.items():
+for items, item in Biography.items():
     print(f"{items}: {item}")
